b5da72c3a79e7188f4cdcd94aea957a021789dcf
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-05 23:30:31 +0300

    add 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,9 +1,42 @@
 include "lists.h"
 
 /**
+  * check_cycle - function in C that checks if a singly linked list
+  * @list: a ponter to structure
   *
+  * Return: 0 if there is no cycle, 1 if there is a cycle
   */
 int check_cycle(listint_t *list)
 {
+	listint_t *ch;
+	listint_t *str;
+
+	ch = list;
+	str = list;
+	for (; list && ch && ch->next; list = list->next, ch = ch->next->next)
+	{
+		
+		
+
+		if (list == ch)
+		{
+			list = str;
+			str =  ch;
+			for (;;)
+			{
+				ch = str;
+				while (ch->next != list && ch->next != str)
+				{
+					ch = ch->next;
+				}
+				if (ch->next == list)
+					break;
+
+				list = list->next;
+			}
+			return (1);
+		}
+	}
+
 	return (0);
 }
