99bff0fe5bbeea00be8d1b4be2370f395140eada
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-07 02:16:26 +0300

    add 6-print file

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+j = 0
+i = 0
+while i < 9:
+    while j < 10:
+        if (i == 8) and (j == 9):
+            print("{}{}".format(i, j))
+        else:
+            print("{}{}".format(i, j), end=", ")
+        j += 1
+    i += 1
+    j = i
