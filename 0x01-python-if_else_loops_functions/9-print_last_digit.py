c97b7d9f28869802437bf4666f8ad5706857af02
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-07 12:56:06 +0300

    Add 9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def print_last_digit(number):
+    if number < 0:
+        number *= -1
+        print(number % 10, end="")
+        return number % 10
+    else:
+        print(number % 10, end="")
+        return number % 10
