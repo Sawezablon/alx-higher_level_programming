3d58e3e96f40f1bbd8abf2c4968841b06558fae3
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-07 12:44:32 +0300

    Edit 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
 
 
+def upper(ch):
+    new = ord(ch)
+    if new > 96 and new < 123:
+        return new - 32
+    else:
+        return new
+
+
 def uppercase(str):
+    empty = ""
     for i in range(len(str)):
-        s = ord(str[i])
-        if s > 96 and s < 123:
-            print("{:c}".format(s - 32), end="" if i < len(str) - 1 else "\n")
-        else:
-            print("{:c}".format(s), end="" if i < len(str) - 1 else "\n")
+        empty += "%c" % upper(str[i])
+    print("{:s}".format(empty))
