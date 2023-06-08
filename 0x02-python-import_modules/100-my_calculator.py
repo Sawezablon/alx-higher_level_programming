3f2ae507cdb83001224978e881aed1a96e5bfdb1
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-08 18:07:36 +0300

    add 100-my_calculator.py file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+
+    len = len(sys.argv) - 1
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if len != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    if sys.argv[2] == "+":
+        sum = add(a, b)
+        print("{} + {} = {}".format(a, b, sum))
+    if sys.argv[2] == "-":
+        sum = sub(a, b)
+        print("{} + {} = {}".format(a, b, sum))
+    if sys.argv[2] == "*":
+        sum = mul(a, b)
+        print("{} + {} = {}".format(a, b, sum))
+    if sys.argv[2] == "/":
+        sum = div(a, b)
+        print("{} + {} = {}".format(a, b, sum))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
--- 0x02-python-import_modules/README.md
+++ 0x02-python-import_modules/README.md
@@ -5,3 +5,7 @@
 3. Program that prints the result of the addition of all arguments
 4. Program that prints all the names defined by the compiled module hidden_4.pyc 
 5. Program that imports the variable a from the file variable_load_5.py and prints its value.
+6. Program that imports all functions from the file calculator_1.py and handles basic operations.
+7. Program that prints #pythoniscool, followed by a new line, in the standard output.
+8. Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:
+9. Program that prints the alphabet in uppercase, followed by a new line.
