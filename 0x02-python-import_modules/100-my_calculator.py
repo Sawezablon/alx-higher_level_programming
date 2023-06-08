85d76ea1ff2c7492a46f72ddbf71cbcaeb7d63ca
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-08 18:10:20 +0300

    Added

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
 
     len = len(sys.argv) - 1
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
     if len != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     if sys.argv[2] == "+":
         sum = add(a, b)
         print("{} + {} = {}".format(a, b, sum))
