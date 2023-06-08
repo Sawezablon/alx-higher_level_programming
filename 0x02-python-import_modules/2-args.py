6a32b6235b02a043872bad3ef0cbf996272b1d7b
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-08 16:04:46 +0300

    Edit 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
-length = len(sys.argv) - 1
-if length > 0:
-    print("{} argument:".format(length))
-else:
-    print("{} argument.".format(length))
+    length = len(sys.argv) - 1
+    if length > 0:
+        print("{} argument:".format(length))
+    else:
+        print("{} argument.".format(length))
 
-for i in range(length):
-    print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    for i in range(length):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
