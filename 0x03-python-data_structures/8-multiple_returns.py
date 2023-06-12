fa2dd2a94f4703a123713484ae33bd0431bbe45b
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-12 11:46:11 +0300

    Add 8-multiple_returns.py file

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    if length != 0:
+        ch = sentence[0]
+        return length, ch
+    
+    return length, None
