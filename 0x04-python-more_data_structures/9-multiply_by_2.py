3f7d746096b990508140190160e09c842d17ab1d
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-14 20:40:54 +0300

    Add 9-multiply_by_2.py

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def multiply_by_2(a_dictionary):
+    new = {}
+    keys = a_dictionary.keys()
+    for key in keys:
+        new[key] = a_dictionary[key] * 2
+
+    return new
