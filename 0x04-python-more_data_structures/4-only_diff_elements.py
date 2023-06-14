e6bfc6349f9ef0ffcb4faab9f8ad6834218a814e
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-14 19:49:17 +0300

    Edit

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -3,4 +3,17 @@
 
 def only_diff_elements(set_1, set_2):
     diff = []
-    for i in set_1
+    common = []
+    for i in set_1:
+        for j in set_2:
+            if i == j:
+                common.append(i)
+    for i in set_1:
+        for j in common:
+            if i != j:
+                diff.append(i)
+    for i in set_2:
+        for j in common:
+            if i != j:
+                diff.append(i)
+    return diff
