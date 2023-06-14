b4103827d352de3ccbe8735a8d0620c6e2516929
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-14 23:11:54 +0300

    Add 12-roman_to_int.py file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if roman_string == None:
+        return 0
     values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
     prev = 0
