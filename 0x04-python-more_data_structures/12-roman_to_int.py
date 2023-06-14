540864090f339d8d819be5b27c879abc75fe8271
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-14 23:06:47 +0300

    Add 12-roman_to_int.py file

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    values = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    prev = 0
+
+    for i in roman_string[::-1]:
+        value = values[i]
+        if value >= prev:
+            num += value
+        else:
+            num -= value
+        prev = value
+
+    return num
