c47d1e49276c824a642a69de84c8099bc2bb0d17
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-27 08:03:45 +0300

    Add

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,11 @@
 #!/usr/bin/bash
 def safe_print_list(my_list=[], x=0):
-    try:
-        i = 0
-        for list in range(x):
+    i = 0
+    for list in range(x):
+        try:
             print("{}".format(my_list[i]), end='')
             i += 1
-        print()
-        return i
-    except IndexError:
-        print()
-        return i
+        except IndexError:
+            break
+    print()
+    return i
