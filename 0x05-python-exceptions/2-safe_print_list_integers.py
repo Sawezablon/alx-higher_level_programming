e75968238a77e897ab93cc6666491eeb39d57817
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-06-27 08:35:20 +0300

    add 2-safe file

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    for list in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end='')
+            i += 1
+        except ValueError:
+            i += 1
+            continue
+        except TypeError:
+            i += 1
+            continue
+    print()
+    return i
