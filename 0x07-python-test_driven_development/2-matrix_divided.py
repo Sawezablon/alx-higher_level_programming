bd243b83a7fc977bfc398d4bd5f5e46c70852230
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-05 14:55:39 +0300

    Add 2-matrix_divided.py file

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module matrix_divided
+divides all elements
+
+"""
+
+
+def matrix_divided(matrix, div):
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = []
+    for i in matrix:
+        if len(matrix[0]) != len(i):
+            raise TypeError("Each row of the matrix must have the same size")
+        temp = []
+        for j in i:
+            if not isinstance(j, (int, float)):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            temp.append(round(j / div, 2))
+        new_matrix.append(temp)
+
+    return new_matrix
