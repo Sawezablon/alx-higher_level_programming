231419cc5404ee9bd18c6405262c2de0cea37701
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-05 23:28:38 +0300

    Edit 5-text_indentation.py file

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -14,13 +14,19 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     i = 0
     while i < len(text):
-        if text[i] == "." or text[i] == "?":
+        if text[i] == "." or text[i] == "?" or text[i] == ":":
             print(text[i])
             print()
             i += 1
+            while True:
+                if i < len(text) and text[i] == " ":
+                    i += 1
+                else:
+                    break
         elif i == len(text) - 1:
             print(text[i])
             print()
+            i += 1
         else:
             print(text[i], end="")
-        i += 1
+            i += 1
