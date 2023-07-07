a65d30b9e09118d3268911dc622062e512958bd3
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-08 02:07:59 +0300

    Add 101-lazy_matrix_mul.py file

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Module lazy_matrix_mul
+function that multiplies 2 matrices 
+
+"""
+import numpy
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    function that multiplies 2 matrices
+    """
+    return numpy.matmul(m_a, m_b)
