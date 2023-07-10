bdc5d9d4b7b6465f613a07b671e45f36af019e54
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-10 17:32:30 +0300

    Edit 2-is_same_class.py file

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def is_same_class(obj, a_class):
     """ function that returns True if the object is exactly"""
-    if not isinstance(obj, a_class):
-        return True
+    return type(obj) == a_class
