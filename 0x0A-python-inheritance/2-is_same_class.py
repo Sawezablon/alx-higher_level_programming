41d07e2cf7cd25abf77c4c8044882fb079cf586a
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-10 17:16:22 +0300

    Add 2-is_same_class.py file

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def is_same_class(obj, a_class):
+    """ function that returns True if the object is exactly"""
+    if isinstance(obj, int):
+        return True
+    else:
+        return False
