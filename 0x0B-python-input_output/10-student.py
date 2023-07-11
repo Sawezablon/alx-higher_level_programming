a8d5594f8318cbda2ef1835fa9c4152e126e7f3f
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-11 23:59:52 +0300

    Add 10-student.py file

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module class Student"""
+
+class Student:
+    """method for class"""
+    def __init__(self, first_name, last_name, age):
+    """Initialization"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """that retrieves a dictionary representation"""
+        if (type(attrs) == list and
+                all(type(obj) == str for obj in attrs)):
+            return {i: getattr(self, i) for i in attrs if hasattr(self, i)}
+        return self.__dict__
