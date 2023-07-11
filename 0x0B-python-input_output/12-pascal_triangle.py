84d121f271cc10df03251e3194dedc068e324d34
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-12 00:10:25 +0300

    Add 12-pascal_triangle.py file

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
 """Module pascal_triangle"""
-import json
 
 
 def pascal_triangle(n):
-    """function that creates an Object from a JSON file"""
-    with open(filename) as file:
-        return json.load(file)
+    """function that returns a list of lists of integers"""
+    if n <= 0:
+        return []
+
+    tri = [[1]]
+    while len(tri) != n:
+        new = tri[-1]
+        temp = [1]
+        for i in range(len(new) - 1):
+            temp.append(new[i] + new[i + 1])
+        temp.append(1)
+        tri.append(temp)
+    return tri
