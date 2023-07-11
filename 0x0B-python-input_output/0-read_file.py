c5f065f7dd05513e078d4eb255bbf36140170085
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-11 16:19:06 +0300

    Add 0-read_file.py file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module read_file"""
+
+
+def read_file(filename=""):
+    """function that reads a text file"""
+    with open("my_file_0.txt", encoding="utf-8") as file:
+        print(file.read().rstrip())
