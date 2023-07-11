22078700777b016585501c0c72762a5296e63aa6
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-07-11 16:25:56 +0300

    Edit 0-read_file.py file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """function that reads a text file"""
     with open("my_file_0.txt", encoding="utf-8") as file:
-        print(file.read().rstrip())
+        print(file.read(), end='')
