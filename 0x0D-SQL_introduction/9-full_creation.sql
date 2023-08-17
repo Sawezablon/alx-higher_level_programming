8b64cc44d06b15afcd6fe48997f6f38a3587eb6b
Zablon Sawe <zablonkiprotich16@gmail.com>
2023-08-18 00:54:50 +0300

    Add 9-full_creation.sql file

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- create second table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+);
+INSERT INTO second_table (id, name, score) VALUES (1, "john", 10);
+INSERT INTO second_table (id, name, score) VALUES (2, "john", 3);
+INSERT INTO second_table (id, name, score) VALUES (3, "john", 14);
+INSERT INTO second_table (id, name, score) VALUES (4, "john", 8);
